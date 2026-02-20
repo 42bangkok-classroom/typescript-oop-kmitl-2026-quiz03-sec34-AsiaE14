dea6129a6a2d65ab12c65ce6c16549e33a63199b
AsiaE14 <sadgudo@gmail.com>
2026-02-20 18:28:21 +0700

    q2

--- src/practicals/p02/user.ts
+++ src/practicals/p02/user.ts
@@ -1,1 +1,28 @@
-export class User {}
+export class User {
+
+    firstname: string |""
+    lastname: string |""
+    age: number |""
+
+    static BIRTH_YEAR = 2000;
+
+    setFirstname(firstname: string){
+        this.firstname = firstname;
+    }
+
+    setLastname(lastname: string){
+        this.lastname = lastname;
+    }
+
+    setAge(age: number){
+        this.age = age;
+    }
+
+    getFullName(){
+        return `${this.firstname} ${this.lastname}`;
+    }
+
+    getAge(){
+        return this.age;
+    }       
+}
